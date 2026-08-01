8b42c9ccc2388dd2c058f1f2ed466be4674ce0e7
Bayu <sbayu8378@gmail.com>
2026-08-01 14:36:44 +0700

    update

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,58 +2,66 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        User::firstOrCreate([
-            'name' => 'Admin',
-            'email' => 'admin@ccrid.com',
-            'password' => Hash::make('password'),
-            'role' => 'admin',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'admin@ccrid.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password'),
+                'role' => 'admin',
+            ]
+        );
 
-        User::firstOrCreate([
-            'name' => 'User',
-            'email' => 'user001@ccrid.com',
-            'password' => Hash::make('user_ccrid1'),
-            'role' => 'viewer',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'user001@ccrid.com'],
+            [
+                'name' => 'User',
+                'password' => Hash::make('user_ccrid1'),
+                'role' => 'viewer',
+            ]
+        );
 
-        User::firstOrCreate([
-            'name' => 'Budi',
-            'email' => 'user002@ccrid.com',
-            'password' => Hash::make('user_ccrid2'),
-            'role' => 'viewer',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'user002@ccrid.com'],
+            [
+                'name' => 'Budi',
+                'password' => Hash::make('user_ccrid2'),
+                'role' => 'viewer',
+            ]
+        );
 
-        User::firstOrCreate([
-            'name' => 'Andi',
-            'email' => 'user003@ccrid.com',
-            'password' => Hash::make('user_ccrid3'),
-            'role' => 'viewer',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'user003@ccrid.com'],
+            [
+                'name' => 'Andi',
+                'password' => Hash::make('user_ccrid3'),
+                'role' => 'viewer',
+            ]
+        );
 
-        User::firstOrCreate([
-            'name' => 'Dina',
-            'email' => 'user004@ccrid.com',
-            'password' => Hash::make('user_ccrid4'),
-            'role' => 'viewer',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'user004@ccrid.com'],
+            [
+                'name' => 'Dina',
+                'password' => Hash::make('user_ccrid4'),
+                'role' => 'viewer',
+            ]
+        );
 
-        User::firstOrCreate([
-            'name' => 'Ani',
-            'email' => 'user005@ccrid.com',
-            'password' => Hash::make('user_ccrid5'),
-            'role' => 'viewer',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'user005@ccrid.com'],
+            [
+                'name' => 'Ani',
+                'password' => Hash::make('user_ccrid5'),
+                'role' => 'viewer',
+            ]
+        );
     }
 }
